23e9d5eff4f31fef7a97faddeb31206b0548d254
Gurudatt Choudhary <119776344+gurudattch@users.noreply.github.com>
2026-06-02 14:59:24 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,10 @@
-"use client";
-
-import { MatrixRain } from "@/components/event/MatrixRain";
-import { Navbar } from "@/components/event/Navbar";
-import { HeroSection } from "@/components/event/HeroSection";
-import { AboutSection } from "@/components/event/AboutSection";
-import { TimelineSection } from "@/components/event/TimelineSection";
-import { EventsSection } from "@/components/event/EventsSection";
-import { CTFSection } from "@/components/event/CTFSection";
-import { GuestsSection } from "@/components/event/GuestsSection";
-import { TeamSection } from "@/components/event/TeamSection";
-import { SponsorsSection } from "@/components/event/SponsorsSection";
-import { RegistrationSection } from "@/components/event/RegistrationSection";
-import { FAQSection } from "@/components/event/FAQSection";
-import { Footer } from "@/components/event/Footer";
-
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-[#0a0a1a]">
-      {/* Matrix Rain Background */}
-      <MatrixRain />
-
-      {/* Navigation */}
-      <Navbar />
-
-      {/* Main Content */}
-      <main className="flex-1 relative z-10">
-        <HeroSection />
-        <AboutSection />
-        <TimelineSection />
-        <EventsSection />
-        <CTFSection />
-        <GuestsSection />
-        <TeamSection />
-        <SponsorsSection />
-        <RegistrationSection />
-        <FAQSection />
-      </main>
-
-      {/* Footer */}
-      <Footer />
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">MU Cyber Events</h1>
+        <p className="text-xl">Event Showcase Site</p>
+      </div>
     </div>
   );
 }
